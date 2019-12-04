830a662a2ed4b04669f10e52acdcdd838ca0cbdb
GRUPOGMV\ggpr <gplaza@gmv.com>
2019-12-04 02:03:39 +0100

    new views

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import render_template
+
+
 app = Flask(__name__)
 
 
@@ -6,7 +9,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-
+@app.route("/template")
+def index():
+    return render_template("index.html")
+	
 @app.route("/about")
 def about():
     return "<h1 style='color: red;'>GGPR WEB IN DOCKER!</h1>"
